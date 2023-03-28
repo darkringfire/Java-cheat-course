5872c3aeefe9a699157f3ec4cd0247c43fcf2c56
darkringfire <darkringfire@gmail.com>
2023-03-28 16:58:34 +0800

    Lesson 1, Task 3

---
+++ src/main/java/lesson_1/task_3/Main.java
@@ -0,0 +1,21 @@
+package lesson_1.task_3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        Integer min1 = null, min2 = null;
+        for (int i = 0; i < n; i++) {
+            int x = in.nextInt();
+            if (min1 == null || x < min1) {
+                min2 = min1;
+                min1 = x;
+            } else if (min2 == null || x < min2) {
+                min2 = x;
+            }
+        }
+        System.out.print(min2);
+    }
+}
