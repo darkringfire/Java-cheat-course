fe93af5e3a7d0c16a8abeedf27bcf5e5d5569c9e
darkringfire <darkringfire@gmail.com>
2023-03-28 16:58:39 +0800

    Lesson 1, Task 4

---
+++ src/main/java/lesson_1/task_4/Main.java
@@ -0,0 +1,32 @@
+package lesson_1.task_4;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        List<Integer> primes = new ArrayList<>(n);
+        int x = 1;
+        primes.add(2);
+        while (n > primes.size()) {
+            x += 2;
+            boolean isPrime = true;
+            for (Integer prime : primes) {
+                if (prime > Math.sqrt(x)) {
+                    break;
+                }
+                if (x % prime == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                primes.add(x);
+            }
+        }
+        System.out.println(primes.get(n-1));
+    }
+}
