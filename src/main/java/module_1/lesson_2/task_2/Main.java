b0ab882a7b1f6873b02352efedf6639fb43b1772
darkringfire <darkringfire@gmail.com>
2023-03-29 12:07:02 +0800

    Module 1. Lesson 2. Task 2.

---
+++ src/main/java/module_1/lesson_2/task_2/Main.java
@@ -0,0 +1,11 @@
+package module_1.lesson_2.task_2;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+    public static void main(String[] args) {
+        logger.info("Hello logs!");
+    }
+}
